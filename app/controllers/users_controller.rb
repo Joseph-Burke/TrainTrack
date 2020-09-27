a8bc75b44e0de8e9c53cb434ae0721666efca4c1
Joseph Burke <joseph_burke@outlook.com>
2020-09-27 09:44:54 +0100

    Add routes and actions to allow a new user to be created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,21 @@
 class UsersController < ApplicationController
-  @users = User.all
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:index]
+  resources :users, only: [:index, :new, :create]
   resources :groups, only: [:index]
   resources :transactions, only: [:index]
   get '/login', to: 'sessions#new'
