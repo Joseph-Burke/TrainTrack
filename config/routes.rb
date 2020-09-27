9e22736a5920249867295966ca7f2c46495c3dbd
Joseph Burke <joseph_burke@outlook.com>
2020-09-27 09:08:09 +0100

    Generate controllers: Users, Groups, Transactions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index]
+  resources :groups, only: [:index]
+  resources :transactions, only: [:index]
 end
