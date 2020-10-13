67817a10682b90e66a6ca0bb1b752ea8171890ec
Joseph Burke <joseph_burke@outlook.com>
2020-10-13 10:13:23 +0100

    Add tests for transactions

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,4 +2,7 @@ class Transaction < ApplicationRecord
   belongs_to :user
   has_many :group_transactions
   has_many :groups, through: :group_transactions
+  validates :name, :description, :amount, presence: true
+  validates :amount, numericality: { greater_than: 0 }
+  validates :description, length: { maximum: 140 }
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create(name: 'Joe') }
+  context 'validation' do
+    subject do
+      described_class.new(
+        user_id: user.id,
+        name: 'Walk the dog',
+        amount: 0.5,
+        description: "Take the dog for a stroll around the neighbourhood"
+      )
+    end
+
+    it 'is valid with requirements' do 
+      expect(subject).to be_valid
+    end
+
+    it 'is invalid without a name present' do
+      subject.name = nil
+      expect(subject).not_to be_valid
+    end
+
+    it 'is invalid without a description present' do
+      subject.description = nil
+      expect(subject).not_to be_valid
+    end
+
+    it 'is invalid without an amount present' do
+      subject.amount = nil
+      expect(subject).not_to be_valid
+    end
+
+    it 'is invalid with a non-positive amount' do
+      subject.amount = 0
+      expect(subject).not_to be_valid
+      subject.amount = -0.5
+      expect(subject).not_to be_valid
+    end
+
+    it 'is invalid without a user_id' do
+      subject.user_id = nil
+      expect(subject).not_to be_valid
+    end
+
+    it 'is invalid with a description over 140 characters long' do
+      subject.description = " " * 141
+      expect(subject).not_to be_valid
+    end
+
+  end
+
 end
